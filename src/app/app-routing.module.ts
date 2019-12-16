7c8093d3b574ccc1694fb2724e345e1484e5778b
Sudesh Wasnik <wasnik353@gmail.com>
2019-12-17 00:16:35 +0530

    16th ka final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   {path : '', component:LoginComponent},
   {path : 'login', component: LoginComponent},
-  {path : 'register', component: RegisterComponent}
+  {path : 'register', component: RegisterComponent},
+  {path: '**', component:LoginComponent}
 ];
 
 @NgModule({
