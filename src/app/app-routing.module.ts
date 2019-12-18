a54883c3a6fce432c4e55d39f430a959217010f9
Sudesh Wasnik <wasnik353@gmail.com>
2019-12-18 20:55:12 +0530

    Added FCM tokens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path : 'login', component: LoginComponent},
   {path : 'register', component: RegisterComponent},
   {path : 'home', component: HomeComponent},
-  {path: '**', component:LoginComponent}
+  // {path: '**', component:LoginComponent}
 ];
 
 @NgModule({
