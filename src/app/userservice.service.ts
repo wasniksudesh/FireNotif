b92b638a3ed63fcce1f46c3dfa929bdc86e3a0a8
Sudesh Wasnik <wasnik353@gmail.com>
2019-12-18 10:11:03 +0530

    This si nice

--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -1,27 +1,41 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestoreModule, AngularFirestore} from '@angular/fire/firestore'
 import {AngularFireModule} from '@angular/fire'
+export interface UserInfo{
+  Devices: [];
+  Friends: [];
+  Requests: [];
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserserviceService {
   constructor(private firestore:AngularFirestore) { }
-  
+  account=this.firestore.collection("Accounts").doc("Accounts")
+  userinfo=this.firestore.collection("UserInfo")
+  final= {  Devices: [],
+    Friends: [],
+    Requests: []};
   getinfo(){
-    return this.firestore.collection("Accounts").doc("Accounts").snapshotChanges();
+    return this.account.snapshotChanges();
   }
   getvalue_getinfo(){
-    console.log('this si getval')
-    return this.firestore.collection("Accounts").doc("Accounts").get().toPromise();
+    console.log('this si fucking ')
+    return this.account.get().toPromise();
   }
   setinfo(data){
-    console.log("inside service file")
-    var keep;
-    // this.getvalue_getinfo().then(res=> console.log("final",res.data()))
-    this.getvalue_getinfo().then(function(text){
-      let poop=text.data();
-      poop.loginmap[data.userid]=[data.password," "];
-      return poop
-    }).then(res=>this.firestore.collection("Accounts").doc("Accounts").set(res)).then(res=>console.log('this is really new for me YAYYA!!!!'));
+    let keyid;
+    this.userinfo.add(this.final).then(function(docref){
+      keyid= docref.id;
+      return keyid
+    }).then(res =>this.getvalue_getinfo()).then(function(text){
+          let poop=text.data();
+          poop.loginmap[data.userid]=[data.password,keyid];
+          return poop
+      }).then(res=>this.account.set(res));
+    
+
   }
 }
